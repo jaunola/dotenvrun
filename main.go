cf7603923f05b4410729663d6fcabea7d44360aa
Joonas Aunola <joonas.aunola@qvik.fi>
2026-07-16 01:23:54 +0300

    update logging and handling

--- main.go
+++ main.go
@@ -11,7 +11,9 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 1 {
+	log.SetFlags(0)
+	log.SetPrefix("--> ")
+	if len(os.Args) < 2 {
 		fmt.Printf("usage: %s <command>\n", os.Args[0])
 		os.Exit(1)
 	}
@@ -31,20 +33,23 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed: %s", err)
 	}
+	envKeys := []string{}
 	bf := bufio.NewScanner(bytes.NewReader(f))
 	for bf.Scan() {
 		s := bf.Text()
 
-		key, val, ok := strings.Cut(s, "=")
+		key, val, ok := getKeyVal(s)
 		if !ok {
 			continue
 		}
 		cmd.Env = append(cmd.Env, key+"="+val)
+		envKeys = append(envKeys, key)
 	}
 	if bf.Err() != nil {
 		log.Fatalf("env parse failed: %s", err)
 	}
-
+	log.Printf("read %d .env variables: %s", len(envKeys), strings.Join(envKeys, ", "))
+	log.Printf("running prog %s...", cmd.Path)
 	// start program and wait
 	if err := cmd.Start(); err != nil {
 		log.Fatalf("cmd run failed: %s", err)
@@ -56,4 +61,21 @@ func main() {
 		os.Exit(ee.ExitCode())
 	}
 	os.Exit(0)
+}
+
+func getKeyVal(line string) (string, string, bool) {
+	key, val, ok := strings.Cut(line, "=")
+	key = strings.TrimSpace(key)
+	val = strings.TrimSpace(val)
+
+	// skip empty keys, values and comments
+	if len(key) == 0 || len(val) == 0 || strings.HasPrefix(key, "#") {
+		return key, val, false
+	}
+
+	if strings.HasPrefix(val, `"`) || strings.HasSuffix(val, `"`) {
+		panic(".env file contains quoted values, please remove them")
+	}
+
+	return key, val, ok
 }
